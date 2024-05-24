194898ab7ee69b75fecec2566d849b27b032b3e2
Ghjulia Sialelli <45049580+ghjuliasialelli@users.noreply.github.com>
2024-05-24 17:22:18 +0200

    Update download.sh

--- Models/download.sh
+++ Models/download.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# Download data for the year 2019
-for i in {0..19}
+years=(2019 2020)
+modes=("test" "train" "val")
+
+# Get the data for year 2019 and 2020
+for year in "${years[@]}"
 do
-  # Construct the URL with the current value of i
-  url="https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/data_subset-2019-v4_${i}-20.h5"
 
-  # Use wget to download the file
-  wget "$url"
-done
+  # Download patches data
+  for i in {0..19}
+  do
+    # Construct the URL with the current value of i
+    url="https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/data_subset-${year}-v4_${i}-20.h5"  
+    wget "$url"
+  done
 
-# Download data for the year 2019
-for i in {0..19}
-do
-  # Construct the URL with the current value of i
-  url="https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/data_subset-2020-v4_${i}-20.h5"
+  # Download tabular data
+  for mode in "${modes[@]}"
+  do
+    wget "https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/${mode}_features_${year}-v4.csv"
+  done
 
-  # Use wget to download the file
-  wget "$url"
 done
 
 # Download the statistics
