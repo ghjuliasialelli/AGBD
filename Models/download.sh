1f8f904ffd926f2cc1d3f1faae43197b3e15b8cf
Ghjulia Sialelli <45049580+ghjuliasialelli@users.noreply.github.com>
2024-05-24 17:40:41 +0200

    Update download.sh

--- Models/download.sh
+++ Models/download.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Create the output folder Data/ if it doesn't exist
+if [ -d "Data" ]; then
+  echo "Directory 'Data' already exists. Doing nothing."
+else
+  mkdir "Data"
+  echo "Directory 'Data' created."
+fi
+
+# Define the years and splits
 years=(2019 2020)
 modes=("test" "train" "val")
 
@@ -12,19 +21,19 @@ do
   do
     # Construct the URL with the current value of i
     url="https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/data_subset-${year}-v4_${i}-20.h5"  
-    wget "$url"
+    wget -P Data/ "$url"
   done
 
   # Download tabular data
   for mode in "${modes[@]}"
   do
-    wget "https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/${mode}_features_${year}-v4.csv"
+    wget -P Data/ "https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/${mode}_features_${year}-v4.csv"
   done
 
 done
 
 # Download the statistics
-wget https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/statistics_subset_2019-2020-v4.pkl
+wget -P Data/ https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/statistics_subset_2019-2020-v4.pkl
 
 # Download the train/test/val split
-wget https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/biomes_splits_to_name.pkl
+wget -P Data/ https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/674193/biomes_splits_to_name.pkl
