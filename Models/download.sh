fb93741d3680693733a440394139c21a7951b1aa
Ghjulia Sialelli <45049580+ghjuliasialelli@users.noreply.github.com>
2024-06-06 15:25:22 +0200

    Update download.sh

--- Models/download.sh
+++ Models/download.sh
@@ -9,7 +9,7 @@ else
 fi
 
 # Define the years and splits
-years=(2019 2020)
+years=("2019" "2020")
 modes=("test" "train" "val")
 
 # Get the data for year 2019 and 2020
@@ -20,20 +20,31 @@ do
   for i in {0..19}
   do
     # Construct the URL with the current value of i
-    url="https://libdrive.ethz.ch/index.php/s/VPio6i5UlXTgir0/download?path=%2F&files=data_subset-${year}-v4_${i}-20.h5"
-    wget -P Data/ "$url"
+    wget --header="Cookie: oc_sessionPassphrase=G58vqE1m1B4pBYVOAnvgNHrLX6tHcaWWPGtOIaL16X0lxxinrTg3off1h7WaVNukrS39Jr1BkhsdJreHtvLeVevT9CrXjr%2BzbE4Nli9dI4BHB1AjqAiLQl82nCZZ0QmC; ocDownloadStarted=gxairgqzc" \
+         --header="Connection: keep-alive" \ 
+         --header="Referer: https://libdrive.ethz.ch/" \
+         "https://libdrive.ethz.ch/index.php/s/VPio6i5UlXTgir0/download?path=%2F&files=data_subset-${year}-v4_${i}-20.h5&downloadStartSecret=gxairgqzc" -O Data/data_subset-${year}-v4_${i}-20.h5
   done
 
   # Download tabular data
   for mode in "${modes[@]}"
   do
-    wget -P Data/ "https://libdrive.ethz.ch/index.php/s/VPio6i5UlXTgir0/download?path=%2F&files=${mode}_features_${year}-v4.csv"
+    wget --header="Cookie: oc_sessionPassphrase=G58vqE1m1B4pBYVOAnvgNHrLX6tHcaWWPGtOIaL16X0lxxinrTg3off1h7WaVNukrS39Jr1BkhsdJreHtvLeVevT9CrXjr%2BzbE4Nli9dI4BHB1AjqAiLQl82nCZZ0QmC; ocDownloadStarted=gxairgqzc" \
+         --header="Connection: keep-alive" \ 
+         --header="Referer: https://libdrive.ethz.ch/" \
+         "https://libdrive.ethz.ch/index.php/s/VPio6i5UlXTgir0/download?path=%2F&files=${mode}_features_${year}-v4.csv&downloadStartSecret=gxairgqzc" -O Data/${mode}_features_${year}-v4.csv
   done
 
 done
 
 # Download the statistics
-wget -P Data/ "https://libdrive.ethz.ch/index.php/s/VPio6i5UlXTgir0/download?path=%2F&files=statistics_subset_2019-2020-v4.pkl"
+wget --header="Cookie: oc_sessionPassphrase=G58vqE1m1B4pBYVOAnvgNHrLX6tHcaWWPGtOIaL16X0lxxinrTg3off1h7WaVNukrS39Jr1BkhsdJreHtvLeVevT9CrXjr%2BzbE4Nli9dI4BHB1AjqAiLQl82nCZZ0QmC; ocDownloadStarted=gxairgqzc" \
+     --header="Connection: keep-alive" \ 
+     --header="Referer: https://libdrive.ethz.ch/" \
+     "https://libdrive.ethz.ch/index.php/s/VPio6i5UlXTgir0/download?path=%2F&files=statistics_subset_2019-2020-v4.pkl&downloadStartSecret=gxairgqzc" -O Data/statistics_subset_2019-2020-v4.pkl
 
 # Download the train/test/val split
-wget -P Data/ "https://libdrive.ethz.ch/index.php/s/VPio6i5UlXTgir0/download?path=%2F&files=biomes_splits_to_name.pkl"
+wget --header="Cookie: oc_sessionPassphrase=G58vqE1m1B4pBYVOAnvgNHrLX6tHcaWWPGtOIaL16X0lxxinrTg3off1h7WaVNukrS39Jr1BkhsdJreHtvLeVevT9CrXjr%2BzbE4Nli9dI4BHB1AjqAiLQl82nCZZ0QmC; ocDownloadStarted=gxairgqzc" \
+     --header="Connection: keep-alive" \ 
+     --header="Referer: https://libdrive.ethz.ch/" \
+     "https://libdrive.ethz.ch/index.php/s/VPio6i5UlXTgir0/download?path=%2F&files=biomes_splits_to_name.pkl&downloadStartSecret=gxairgqzc" -O Data/biomes_splits_to_name.pkl
